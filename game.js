2160769176e1ab3dbd5815a9c21afa97f4da3034
Neeraj-Mirji <neerajmirji@gmail.com>
2021-03-19 11:02:45 +0530

    final update

---
+++ game.js
@@ -0,0 +1,82 @@
+
+var buttonColor = ["red" , "blue" , "green" , "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+$("body").keydown(function()
+{
+  if(level == 0) nextSequence();
+});
+
+$(".btn").click(handler);
+
+
+function nextSequence()
+{
+  level++;
+  $("h1").text("level " + level);
+
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColor = buttonColor[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeOut(150).fadeIn(150);
+  playSound(randomChosenColor);
+}
+
+
+
+
+function handler()
+{
+  var userChosenColor = event.target.id;
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  var lastIndex = userClickedPattern.length - 1;
+  checkAnswer(lastIndex);
+}
+
+
+function checkAnswer(currentLevel)
+{
+ if(userClickedPattern[currentLevel] == gamePattern[currentLevel])
+ {
+    if(userClickedPattern.length == gamePattern.length)
+      {
+       userClickedPattern = [];
+       setTimeout(nextSequence,1000);
+      }
+ }
+ else
+ {
+   userClickedPattern = [];
+   level = 0;
+   gamePattern = [];
+   $("h1").text("Game Over! Press any to restart. ")
+   playSound("wrong");
+   $("body").addClass("game-over");
+   setTimeout(function()
+                     {
+                      $("body").removeClass("game-over");
+                     },200);
+ }
+}
+
+
+function playSound(name)
+{
+  var randomMusic = new Audio("sounds/" + name + ".mp3");
+  randomMusic.play();
+}
+
+
+function animatePress(currentColor)
+{
+  var currentButton =   $("#" + currentColor);
+  currentButton.addClass("pressed");
+  setTimeout(function()
+                 {
+                   currentButton.removeClass("pressed");
+                  },100);
+}
